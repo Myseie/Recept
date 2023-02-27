6cb8fee6c5464a39ddba3775fe41a1ac5500cd09
Myseie <emil_westerholm@hotmail.com>
2023-02-27 16:47:17 +0100

    Initial commit

---
+++ Models/Database.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Recept.Models
+{
+    public class Database
+    {
+        public List<Recept> GetRecept()
+        {     
+        string connectionString = "Data Source=localhost;Initial Catalog=RecipeDB;Integrated Security=True";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = "SELECT * FROM Recipe";
+
+            var reader = cmd.ExecuteReader();
+
+            var recept = new List<Recept>();
+
+            while (reader.Read())
+            {
+                int = int.Parse(reader[]
+            }
+
+        }
+       
+    }
+}
---
+++ Models/Recept.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Recept.Models
+{
+    public class Recept
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Summary { get; set; }
+
+    }
+}
